d025736fe69bf4c52ef8d22a7c09470e30dbc3d0
Jon Powers <jbpowers9@yahoo.com>
2026-03-21 16:03:37 -0400

    Initial Commit

---
+++ ErgoraLiDAR/ErgoraLiDARApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErgoraLiDARApp.swift
+//  ErgoraLiDAR
+//
+//  Created by Deborah Powers on 3/21/26.
+//
+
+import SwiftUI
+
+@main
+struct ErgoraLiDARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
